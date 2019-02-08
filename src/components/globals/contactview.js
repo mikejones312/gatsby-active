f4639395cd07159417be2aaee4035669063401cb
mikejones312 <46551039+mikejones312@users.noreply.github.com>
2019-02-08 06:43:50 -0800

    Update contactview.js

--- src/components/globals/contactview.js
+++ src/components/globals/contactview.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react'
 import { Link } from 'gatsby'
 
 class weWrite extends Component {
+    oncontactviewcolse=(e)=>{
+        document.getElementById("contactView").classList.remove("is-active");
+    }
     render() {
         return (
-
             <div id="contactView" className="quickview has-background-info has-text-white">
             <header className="quickview-header has-background-dark">
                 <p className="title has-text-white has-text-weight-bold">
@@ -13,36 +15,25 @@ class weWrite extends Component {
                     </span>
                     Speak with an Expert
             </p>
-                <span className="delete" data-dismiss="quickview" onclick="this.parentElement.parentElement.classList.remove('is-active')"></span>
+                <span className="delete" data-dismiss="quickview" onClick={this.oncontactviewcolse}></span>
             </header>
 
             <div className="quickview-body">
                 <div className="quickview-block">
                     <form action="/" method="post">
                         <input type="hidden" name="zlf" value="side-form" />
-                      
-                   
-
-
-                      
-
-
                         <div className="field is-grouped">
                             <div className="control">
-                                <button className="button is-primary" type="submit" onclick="sendLandingPageEventToGoogles('form_submission')">Submit</button>
+                                <button className="button is-primary" type="submit" onClick="sendLandingPageEventToGoogles('form_submission')">Submit</button>
                             </div>
                             <div className="control">
-                                <button className="button is-text" onclick="document.querySelector('.quickview').classList.remove('is-active')">Cancel</button>
+                                <button className="button is-text" onClick={this.oncontactviewcolse}>Cancel</button>
                             </div>
                         </div>
-                    </form>
-                   
+                    </form>                   
                 </div>
-
             </div>
-
             <footer className="quickview-footer has-background-dark has-text-light has-text-centered">
-              
         </footer>
         </div>
 
